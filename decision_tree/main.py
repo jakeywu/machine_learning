8f11b4d969630a2d9ba02687b579a6c522de4c09
吴万杰 <wanjie.wu@socialcredits.cn>
2018-10-19 17:36:01 +0800

    shannon

--- decision_tree/main.py
+++ decision_tree/main.py
@@ -1,5 +1,7 @@
 # coding:utf8
 
+import numpy as np
+from collections import Counter
 from math import log
 
 
@@ -11,46 +13,39 @@ def example_data_set():
     类别(是否给贷款): no代表否　yes代表是
     """
     input_x = [
-        [0, 0, 0, 0, 'no'],
-        [0, 0, 0, 1, 'no'],
-        [0, 1, 0, 1, 'yes'],
-        [0, 1, 1, 0, 'yes'],
-        [0, 0, 0, 0, 'no'],
-        [1, 0, 0, 0, 'no'],
-        [1, 0, 0, 1, 'no'],
-        [1, 1, 1, 1, 'yes'],
-        [1, 0, 1, 2, 'yes'],
-        [1, 0, 1, 2, 'yes'],
-        [2, 0, 1, 2, 'yes'],
-        [2, 0, 1, 1, 'yes'],
-        [2, 1, 0, 1, 'yes'],
-        [2, 1, 0, 2, 'yes'],
-        [2, 0, 0, 0, 'no']
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0],
+        [0, 1, 0, 1, 1],
+        [0, 1, 1, 0, 1],
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 1, 0],
+        [1, 1, 1, 1, 1],
+        [1, 0, 1, 2, 1],
+        [1, 0, 1, 2, 1],
+        [2, 0, 1, 2, 1],
+        [2, 0, 1, 1, 1],
+        [2, 1, 0, 1, 1],
+        [2, 1, 0, 2, 1],
+        [2, 0, 0, 0, 0]
     ]
     title = ['年龄', '有工作', '有自己的房子', '信贷情况']
-    return input_x, title
+    return np.array(input_x), title
 
 
-def calcShannonEnt(dataSet):
-    # 返回数据集行数
-    numEntries=len(dataSet)
-    #保存每个标签（label）出现次数的字典
-    labelCounts={}
-    #对每组特征向量进行统计
-    for featVec in dataSet:
-        currentLabel=featVec[-1]                     #提取标签信息
-        if currentLabel not in labelCounts.keys():   #如果标签没有放入统计次数的字典，添加进去
-            labelCounts[currentLabel]=0
-        labelCounts[currentLabel]+=1                 #label计数
+def calculate_shannon_entropy(data_set):
+    """
+    计算信息熵
+    """
+    total_count = data_set.shape[0]
+    shannon_entropy = 0.
+    for key, v in Counter(data_set[:, -1]).items():
+        prob = v/total_count
+        print("类别{}的信息量为{}".format(key, -log(prob, 2)))
+        shannon_entropy += -prob*log(prob, 2)
+    return shannon_entropy
 
-    shannonEnt=0.0                                   #经验熵
-    #计算经验熵
-    for key in labelCounts:
-        prob=float(labelCounts[key])/numEntries      #选择该标签的概率
-        shannonEnt-=prob*log(prob,2)                 #利用公式计算
-    return shannonEnt                                #返回经验熵
 
-# if __name__=='__main__':
-#     dataSet,features=creatDataSet()
-#     print(dataSet)
-#     print(calcShannonEnt(dataSet))
+if __name__ == '__main__':
+    x, y = example_data_set()
+    print(calculate_shannon_entropy(x))
