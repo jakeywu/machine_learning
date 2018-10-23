b467b8a9d811fca235d1bfa7b27967d40c1da4c4
吴万杰 <wanjie.wu@socialcredits.cn>
2018-10-23 11:36:58 +0800

    决策树算法实现

--- decision_tree/main.py
+++ decision_tree/main.py
@@ -41,11 +41,35 @@ def calculate_shannon_entropy(data_set):
     shannon_entropy = 0.
     for key, v in Counter(data_set[:, -1]).items():
         prob = v/total_count
-        print("类别{}的信息量为{}".format(key, -log(prob, 2)))
         shannon_entropy += -prob*log(prob, 2)
     return shannon_entropy
 
 
+def choose_best_feature(data_set, label):
+    """
+    通过信息增益选择最优特征
+    """
+    features_num = data_set.shape[1] - 1
+    base_shannon_entropy = calculate_shannon_entropy(data_set)
+    print("数据集整体经验熵为%.3f\n" % base_shannon_entropy)
+    for i in range(features_num):
+        values = list(set(data_set[:, i]))
+        entropy = 0.
+        for j in values:
+            sub_set = data_set[data_set[:, i] == j]  # 获取子集
+            sub_shannon = calculate_shannon_entropy(sub_set)  # 子集条件熵
+            prob = sub_set.shape[0] / data_set.shape[0]
+            entropy += prob * sub_shannon
+        print("[%s]信息增益为%.3f" % (label[i], base_shannon_entropy - entropy))
+
+
+def create_decision_tree():
+    """
+    构造决策树
+    """
+    pass
+
+
 if __name__ == '__main__':
     x, y = example_data_set()
-    print(calculate_shannon_entropy(x))
+    choose_best_feature(x, y)
